0c7bcec922dac33cf8e82e2a00f448de28db1c16
Dave Turner <davetii@gmail.com>
2023-10-22 20:15:20 -0400

    Implementing all people

--- src/main/java/software/daveturner/helloopenapi/delegate/V1ApiDelegateImpl.java
+++ src/main/java/software/daveturner/helloopenapi/delegate/V1ApiDelegateImpl.java
@@ -3,11 +3,35 @@ package software.daveturner.helloopenapi.delegate;
 import org.springframework.http.*;
 import org.springframework.stereotype.*;
 import software.daveturner.helloopenapi.api.*;
+import software.daveturner.helloopenapi.model.*;
+
+import java.util.*;
 
 @Component
 public class V1ApiDelegateImpl  implements V1ApiDelegate {
+
+    Map<String, Person> people = new HashMap<>();
+    public V1ApiDelegateImpl() {
+        people.put("Diana", newPerson("Diana", "Wife"));
+        people.put("Mary", newPerson("Mary", "Daughter"));
+        people.put("David", newPerson("David", "Son"));
+        people.put("Wendie", newPerson("Wendie", "Sister"));
+    }
+
+    private Person newPerson(String n, String r) {
+        Person p = new Person();
+        p.setName(n);
+        p.setRole(r);
+        return p;
+    }
+
     @Override
     public ResponseEntity<String> sayHello(String name) {
         return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Person>> getAllPersons() {
+        return ResponseEntity.ok(new ArrayList<Person>(people.values()));
     }
 }
