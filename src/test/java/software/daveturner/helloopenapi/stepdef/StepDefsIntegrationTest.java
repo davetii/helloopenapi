b1a8eebc3ff5b5c38c5ed2afa3c099d813821e1a
Dave Turner <davetii@gmail.com>
2023-10-29 14:05:50 -0400

    [DaveT] rolling back to mvc based version

--- src/test/java/software/daveturner/helloopenapi/stepdef/StepDefsIntegrationTest.java
+++ src/test/java/software/daveturner/helloopenapi/stepdef/StepDefsIntegrationTest.java
@@ -1,21 +1,28 @@
-package software.daveturner.helloopenapi.stepdef;
+package software.daveturner.helloopenapi.cucumberglue;
 
 import io.cucumber.java.en.*;
 import org.junit.jupiter.api.*;
-import reactor.core.publisher.*;
+import org.springframework.boot.test.context.*;
 import software.daveturner.helloopenapi.delegate.*;
 import software.daveturner.helloopenapi.model.*;
 
+import java.util.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@SpringBootTest
+
 public class StepDefsIntegrationTest {
 
-    private final V1ApiDelegateImpl d = new V1ApiDelegateImpl();
+    V1ApiDelegateImpl d = new MockV1ApiDelegateImpl();
     private String greetingValue = null;
     private int allPeopleSize = 0;
 
     private int status;
     @When("user {string} says hello")
     public void userCallsSayHello(String newName) {
-        greetingValue = d.sayHello(newName, null).block().getBody();
+        greetingValue = d.sayHello(newName).getBody();
     }
 
     @Then("api returns {string}")
@@ -25,7 +32,7 @@ public class StepDefsIntegrationTest {
 
     @When("getAllPersons is called")
     public void callGetAllPlayers() {
-        allPeopleSize = d.getAllPersons(null).block().getBody().collectList().block().size();
+        allPeopleSize = d.getAllPersons().getBody().size();
     }
 
     @Then("size returned is 4")
@@ -34,16 +41,28 @@ public class StepDefsIntegrationTest {
     }
 
     @When("newPerson is called with {string} and {string}")
-    public void callNewPlayer(String name, String role) {
+    public void callNewPlayer(String name, String role) throws Exception {
         Person p = new Person();
         p.setRole(role);
         p.setName(name);
-        status = d.createPerson(Mono.just(p), null).block().getStatusCode().value();
+        status = d.createPerson(p).getStatusCode().value();
+        System.out.println("status: " + status );
     }
 
     @Then("api returns status {string}")
     public void ensureGetNewPersonReturnsExpected(String expected) {
         Assertions.assertEquals(status, Integer.parseInt(expected));
     }
+
+    class MockV1ApiDelegateImpl extends V1ApiDelegateImpl {
+
+
+        public List<String> readRoles() {
+            String [] s = {"sibling", "spouse", "parent", "child", "friend"};
+            return Arrays.asList(s);
+        }
+
+    }
+
 
 }
